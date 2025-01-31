d609f3b6bf42e4c5df9269bc56b051c0de4da970
Michael Roberts - CSAcademyIO <m.roberts94@hotmail.co.uk>
2025-01-31 03:38:40 +0000

    Add Base UILogic

--- bank/ui/app/page.tsx
+++ bank/ui/app/page.tsx
@@ -1,101 +1,108 @@
-import Image from "next/image";
 
+import { useState, useCallback, useRef, useEffect } from "react";
+import { motion } from "framer-motion";
+import dynamic from "next/dynamic";
+
+import { Spinner } from "@radix-ui/themes";
+import { menu } from "@/app/Navbar";
+import { AiFillInstagram, AiFillDiscord, AiOutlineArrowUp } from "react-icons/ai";
+const NavbarNoSSR = dynamic(() => import("@/app/Navbar"), { ssr: false });
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  
+    
+  const [walletAddress, setWalletAddress] = useState<string>("");
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [availableNfts, setAvailableNfts] = useState<number>(9999);
+  //
+  //Handle Accounts Changed
+  const handleAccountsChanged = (accounts: string[]) => {
+    setWalletAddress(accounts.length > 0 ? accounts[0] : "");
+  };
+  //Fetch Crypto Wallets:
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  //Connect Wallet Accounts
+  const connectWallet = async () => {
+    
+  };
+
+  //Get current wallet
+  const GetCurrentWallet = useCallback(async () => {
+    
+  }, []);
+
+  // Add Wallet Listener
+
+  const AddWalletListener = useCallback(async () => {
+    
+  }, []);
+
+  //Section Refs
+  const sectionRef = useRef<HTMLDivElement>({} as HTMLDivElement);
+  const sectionRefs = menu.reduce((menu, item) => {
+    menu[item.id] = sectionRef;
+    return menu;
+  }, {} as Record<string, React.RefObject<HTMLDivElement>>);
+
+  const scrollToSection = (id: string) => {
+    const element = document.getElementById(id);
+    element?.scrollIntoView({ behavior: "smooth" });
+  };
+  useEffect(() => {
+   
+  }, [GetCurrentWallet, AddWalletListener, walletAddress]);
+  return (
+    <div className="min-h-screen min-w-max p-8 pb-16 ">
+      <main className="flex flex-row items-center justify-between space-x-3">
+        <section id="top">
+          <NavbarNoSSR scrollToSection={scrollToSection} />
+        </section>
+
+        {/* Connect and social tags */}
+        <div className="flex flex-row items-center justify-evenly">
+          <AiFillInstagram size={34} />
+          <AiFillDiscord size={34} />
+          <motion.button
+            disabled={isLoading}
+            style={{ border: "4px solid white" }}
+            onClick={connectWallet}
+            className="display: flex flex-row w-32 h-12 mx-4 items-center justify-center font-bold border rounded-md p-3 hover:cursor-pointer bg-pink-700"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <p className="font-HoneyBear text-xs tracking-widest ">
+              {isLoading ? (
+                <Spinner size={"2"} />
+              ) : walletAddress.length > 0 ? (
+                `${walletAddress.substring(0, 3)}...${walletAddress.substring(
+                  walletAddress.length - 5
+                )}`
+              ) : (
+                "Connect"
+              )}
+            </p>
+          </motion.button>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+      {menu.map((section) => (
+        <section
+          key={section.id}
+          id={section.id}
+          ref={sectionRefs[section.id]}
+          className="min-h-screen p-5 flex flex-col items-center justify-between space-x-3"
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          {section.component}
+        </section>
+      ))}
+
+      <motion.button
+        onClick={() => scrollToSection("top")}
+        whileHover={{ scale: 1.1 }}
+        className="fixed"
+        style={{ bottom: "10%", right: "5%" }}
+      >
+        <AiOutlineArrowUp
+          size={35}
+          className="bg-orange-700 border rounded-md"
+        />
+      </motion.button>
     </div>
   );
 }
